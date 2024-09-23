a6b8cc9a43d5ac1c54b3a81d3915b2a369fe2889
Mahindra kumar <mahindra8252@gmail.com>
2024-09-23 09:51:19 +0530

    add cloudnary and api

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //make method and export
 
 const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).
         catch((err)=>next(err))
     }
